bfb4d9616cd7de62e0641adc8f743194f35f7b63
Guilherme Duarte Toffoli <90780008+guilhermetoffoli@users.noreply.github.com>
2024-04-05 19:09:41 -0300

    Add Persist

--- app/src/main/java/Persistencia/DadosUsers.java
+++ app/src/main/java/Persistencia/DadosUsers.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Persistencia;
 
-/**
- *
- * @author Guilherme
- */
+import Modelo.*;
+import java.util.ArrayList;
+
 public class DadosUsers {
+    private static ArrayList<User> usersList;
     
+    static{
+        usersList = (ArrayList<User>)Persist.recuperar("usuarios.dat");
+        if(usersList == null)
+            usersList = new ArrayList<User>();
+    }
+    
+    public static void cadastrar(User u){
+        usersList.add(u);
+        boolean r = Persist.gravar(u, "usuarios.dat");
+    }
 }
